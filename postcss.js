be46355fb239a4dc29165befde9c14159af27bc8
YS SUPREETH <168265763+SrinivasaSupreeth@users.noreply.github.com>
2026-03-19 17:21:38 +0530

    Add files via upload

---
+++ postcss.js
@@ -0,0 +1,6 @@
+var R=require("./chunks/[turbopack]_runtime.js")("postcss.js")
+R.c("chunks/[turbopack-node]_transforms_postcss_ts_0otmkxz._.js")
+R.c("chunks/[root-of-the-server]__0a6enmr._.js")
+R.m("[turbopack-node]/child_process/globals.ts [postcss] (ecmascript)")
+R.m("[turbopack-node]/child_process/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Desktop/KalNet Supreeth/kalnet-supreeth/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/child_process/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
+module.exports=R.m("[turbopack-node]/child_process/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Desktop/KalNet Supreeth/kalnet-supreeth/postcss.config.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/child_process/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
